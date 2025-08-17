20eaf0f54a67def8d642b205ab96b792c49a21dc
Domas Leščinskas <domas.lescinskas@gmail.com>
2025-08-17 12:56:16 +0300

    Fixed project generation Tweaked template CMakeLists.txt

--- scripts/games/GenerateGame.bat
+++ scripts/games/GenerateGame.bat
@@ -11,16 +11,14 @@ set "GAME_NAME=%~1"
 set "GAME_DISPLAY_NAME=%~2"
 if "!GAME_DISPLAY_NAME!"=="" set "GAME_DISPLAY_NAME=!GAME_NAME!"
 
-for %%A in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do (
-    for %%B in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-        set "GAME_NAME_UPPER=!GAME_NAME_UPPER:%%A=%%B!"
-    )
-)
-set "GAME_NAME_UPPER=!GAME_NAME:%GAME_NAME%=!"
-call :ToUpper GAME_NAME_UPPER "%GAME_NAME%"
+set "GAME_NAME_UPPER=!GAME_NAME!"
+call :ToUpper GAME_NAME_UPPER
 
 set "SCRIPT_DIR=%~dp0"
 set "PROJECT_ROOT=%SCRIPT_DIR%..\.."
+pushd "%PROJECT_ROOT%"
+set "PROJECT_ROOT=%CD%"
+popd
 set "TEMPLATE_DIR=%PROJECT_ROOT%\config\games"
 set "GAMES_DIR=%PROJECT_ROOT%\games"
 set "TARGET_DIR=%GAMES_DIR%\%GAME_NAME%"
@@ -43,8 +41,10 @@ for /r "%TEMPLATE_DIR%" %%f in (*) do (
     
     for %%d in ("!target_file!") do mkdir "%%~dpd" 2>nul
     
-    echo !file! | findstr /r "\.in$" >nul
-    if !errorlevel! equ 0 (
+    set "is_template=0"
+    if "!file:~-3!"==".in" set "is_template=1"
+    
+    if "!is_template!"=="1" (
         set "target_clean=!target_file:.in=!"
         powershell -Command "(Get-Content '!file!') -replace '@GAME_NAME@', '%GAME_NAME%' -replace '@GAME_DISPLAY_NAME@', '%GAME_DISPLAY_NAME%' -replace '@GAME_NAME_UPPER@', '!GAME_NAME_UPPER!' | Set-Content '!target_clean!' -Encoding UTF8"
         echo   Generated: !target_clean!
@@ -57,6 +57,10 @@ for /r "%TEMPLATE_DIR%" %%f in (*) do (
 echo.
 echo Game '%GAME_NAME%' created successfully!
 echo Location: %TARGET_DIR%
+
+rem Create empty assets directory
+mkdir "%TARGET_DIR%\assets" 2>nul
+
 echo.
 echo To build your game:
 echo   cmake -B build -S .
@@ -65,11 +69,14 @@ echo   cmake --build build
 goto :eof
 
 :ToUpper
-set "%~1="
-for /f "skip=2 delims=" %%I in ('tree "\%~2"') do if not defined %~1 set "%~1=%%~I"
-for %%A in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do (
-    for %%B in (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) do (
-        call set "%~1=%%%~1:%%A=%%B%%"
-    )
+setlocal enableextensions enabledelayedexpansion
+set "str=!%~1!"
+set "upper=ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+set "lower=abcdefghijklmnopqrstuvwxyz"
+for /L %%i in (0,1,25) do (
+    call set "from=%%lower:~%%i,1%%"
+    call set "to=%%upper:~%%i,1%%"
+    call set "str=%%str:!from!=!to!%%"
 )
+endlocal & set "%~1=%str%"
 goto :eof
