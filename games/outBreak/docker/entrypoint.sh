1c3e27cb951113e05111660ed007e3e6557318c1
Domas Leščinskas <domas.lescinskas@gmail.com>
2025-08-31 15:58:53 +0300

    Updated CMake and Docker configs to take use of hashed filenames to prevent unnecessary caching

--- games/outBreak/docker/entrypoint.sh
+++ games/outBreak/docker/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+set -e
+cd /usr/share/nginx/html
+
+# Find hash-named HTML
+HASHED_HTML=$(ls *.html | head -n 1)
+
+# Symlink to index.html
+ln -sf "$HASHED_HTML" index.html
 nginx -g "daemon off;"
