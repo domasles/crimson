211b9fb3b1656eefa259a5161112fcbd33b04f33
Domas Leščinskas <domas.lescinskas@gmail.com>
2025-08-31 21:33:03 +0300

    Updated Dockerfile to use BUILD_HASH passed as an argument or an environment variable Updated GitHub action to pass the commit hash as an argument Made the built html be renamed to index.html instead of making symlinks Updated nginx.conf

--- games/outBreak/docker/entrypoint.sh
+++ games/outBreak/docker/entrypoint.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-set -e
-cd /usr/share/nginx/html
-
-# Read hash from file
-if [ -f build_hash.txt ]; then
-  HASHED_HTML=$(cat build_hash.txt).html
-else
-  echo "build_hash.txt not found, defaulting to index.html"
-  HASHED_HTML="index.html"
-fi
-
-# Symlink it
-ln -sf "$HASHED_HTML" index.html
-
-# Start nginx
 nginx -g "daemon off;"
