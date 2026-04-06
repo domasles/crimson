6dc8c89d88893ae65421cd8dfebc2e842c16112f
Domas Leščinskas <domas.lescinskas@gmail.com>
2026-04-06 19:16:46 +0300

    Fixed a font not found error in OutBreak

--- games/outBreak/src/scenes/StartScene.cpp
+++ games/outBreak/src/scenes/StartScene.cpp
@@ -7,7 +7,7 @@ using namespace engine::ui;
 
 namespace outBreak {
     void StartScene::init() {
-        setFont("assets/fonts/inter.ttf", true);
+        setFont("assets/fonts/inter/inter.ttf", true);
 
         m_Document.setDocument("assets/ui/start.rml");
         m_Document.listen("play-btn", "click", []() { switchToScene("GameScene"); });
